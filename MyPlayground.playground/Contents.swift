df07f8c862e188f271097637813c0724b3a7097e
Ben Hill <benhill@stagey.net>
2018-08-18 06:55:46 -0700

    Playground

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,35 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//I am a string
+let str: String = "monkey"
+
+//I am an array
+var arr: [Int] = [123, 345]
+var arrNew: Array<Int> = [123, 345]
+arr[0]
+arrNew[0]
+
+//I am a set
+var set: Set<String> = ["foo", "boo"]
+set.count
+
+//I am a dictionary
+var dict: [String:Int] = ["goodbye": 1, "monkey": 2]
+dict["goodbye"]
+
+let myString = String("Hello")
+let myInt = Int(1)
+
+
+
+
+
+
+
+
+
+
+
+
