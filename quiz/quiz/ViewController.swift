26867bf03f51eb98dc87d500f1e52b1d8ad582f6
Ben Hill <benhill@stagey.net>
2018-08-18 06:20:56 -0700

    initial commit

---
+++ quiz/quiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  quiz
+//
+//  Created by Ben Hill on 8/18/18.
+//  Copyright © 2018 stagey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
