8fec1bcb35020a7008f2d5e5c410835b37c47c70
Ben Hill <benhill@stagey.net>
2018-10-01 17:33:27 -0700

    Photo Picker

--- Homepwner/Homepwner/DetailViewController.swift
+++ Homepwner/Homepwner/DetailViewController.swift
@@ -1,14 +1,36 @@
 import UIKit
 
-class DetailViewController: UIViewController{
+class DetailViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate{
     
     @IBOutlet var nameField: UITextField!
     @IBOutlet var dateLabel: UILabel!
     @IBOutlet var valueField: UITextField!
     @IBOutlet var serialNumberField: UITextField!
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var takePicture: UIBarButtonItem!
+    
+    @IBAction func takePicture(_ sender: UIBarButtonItem) {
+        let imagePicker = UIImagePickerController()
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            imagePicker.sourceType = .camera
+        } else{
+            imagePicker.sourceType = .photoLibrary
+        }
+        
+        imagePicker.delegate = self
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
     
     var item: Item!
     let numberFormatter = NumberFormatter()
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        imageView.image = image
+        dismiss(animated: true, completion: nil)
+    }
 
     override func viewDidAppear(_ animated: Bool) {
         
