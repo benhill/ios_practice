7951c72d1ead866621ded410a17cdbd5df4e34e9
Ben Hill <benhill@stagey.net>
2018-10-24 20:22:48 -0700

    Finishing API Call

--- Photorama/Photorama/PhotoStore.swift
+++ Photorama/Photorama/PhotoStore.swift
@@ -1,4 +1,13 @@
-import Foundation
+import UIKit
+
+enum ImageResult{
+    case success(UIImage)
+    case failure(Error)
+}
+
+enum PhotoError: Error {
+    case imageCreationError
+}
 
 enum PhotoResult {
     case success([Photo])
@@ -6,10 +15,46 @@ enum PhotoResult {
 }
 
 class PhotoStore {
+    
     private let session: URLSession = {
         let config = URLSessionConfiguration.default
         return URLSession(configuration: config)
     }()
+    
+    func fetchImage(for photo: Photo, completion: @escaping (ImageResult) -> Void) {
+        let photoURL = photo.remoteURL
+        
+        let request = URLRequest(url: photoURL)
+        
+        let task = session.dataTask(with: request) {
+            (data, response, error) -> Void in
+            
+            let result = self.processImageRequest(data: data, error: error)
+            OperationQueue.main.addOperation {                
+                completion(result)
+            }
+            
+        }
+        task.resume()
+
+    }
+    
+    private func processImageRequest(data: Data?, error: Error?) -> ImageResult {
+        guard
+            let imageData = data,
+            let image = UIImage(data: imageData) else{
+                
+                //couldn't create image
+                if data == nil{
+                    return .failure(error!)
+                } else {
+                    return .failure(PhotoError.imageCreationError)
+                }
+        }
+        return .success(image)
+    }
+    
+
     
     func fetchInterestingPhotos(completion: @escaping (PhotoResult) -> Void){
         let url = FlickrAPI.interestingPhotosURL
@@ -18,7 +63,9 @@ class PhotoStore {
             (data, response, error) -> Void in
             
             let result = self.processPhotoRequest(data: data, error: error)
-            completion(result)
+            OperationQueue.main.addOperation {
+                completion(result)
+            }
         }
         task.resume()
     }
--- Photorama/Photorama/PhotosViewController.swift
+++ Photorama/Photorama/PhotosViewController.swift
@@ -1,8 +1,22 @@
 import UIKit
 
 class PhotosViewController: UIViewController {
+    
     @IBOutlet var imageView: UIImageView!
     var store: PhotoStore!
+    
+    func updateImageView(for photo:Photo) {
+        store.fetchImage(for: photo) {
+            (imageResult) -> Void in
+            
+            switch imageResult {
+                case let .success(image):
+                    self.imageView.image = image
+                case let .failure(error):
+                    print("Error downloading image \(error)")
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -13,6 +27,9 @@ class PhotosViewController: UIViewController {
             switch photoResult {
             case let .success(photos):
                 print("Successfully found \(photos.count) photos.")
+                if let firstPhoto = photos.first {
+                    self.updateImageView(for: firstPhoto)
+                }
             case let .failure(error):
                 print("Error fetching interesting photos \(error)")
             }
